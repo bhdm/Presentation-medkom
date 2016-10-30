7c13b1b7cbc41058a0eedd5535942315fd0ad076
bhdm <bhd.m@ya.ru>
2016-10-31 00:12:40 +0300

    u

--- Class/ItogClass.php
+++ Class/ItogClass.php
@@ -172,6 +172,70 @@ class ItogClass
 #### Вторая колонка ####
 ########################
 
+//      Аллергии-ГКИ-0-Специализация
+        $this->slide5['i14'] = ($this->slide5['c3'] == 2 ? $this->slide4['o19'] + $this->slide4['o30'] : ($this->slide5['b3'] == 1 ? $this->slide4['m19'] + $this->slide4['m30'] : ($this->slide5['b4'] == 1 ? $this->slide4['n19'] + $this->slide4['n30'] : ($this->slide4['l19'] + $this->slide4['l30']))));
+        $this->slide5['i15'] = ($this->slide5['c3'] == 2 ? $this->slide4['o20'] + $this->slide4['o31'] : ($this->slide5['b3'] == 1 ? $this->slide4['m20'] + $this->slide4['m31'] : ($this->slide5['b4'] == 1 ? $this->slide4['n20'] + $this->slide4['n31'] : ($this->slide4['l20'] + $this->slide4['l31']))));
+        $this->slide5['i16'] = ($this->slide5['c3'] == 2 ? $this->slide4['o21'] + $this->slide4['o32'] : ($this->slide5['b3'] == 1 ? $this->slide4['m21'] + $this->slide4['m32'] : ($this->slide5['b4'] == 1 ? $this->slide4['n21'] + $this->slide4['n32'] : ($this->slide4['l21'] + $this->slide4['l32']))));
+        $this->slide5['i17'] = ($this->slide5['c3'] == 2 ? $this->slide4['o22'] + $this->slide4['o33'] : ($this->slide5['b3'] == 1 ? $this->slide4['m22'] + $this->slide4['m33'] : ($this->slide5['b4'] == 1 ? $this->slide4['n22'] + $this->slide4['n33'] : ($this->slide4['l22'] + $this->slide4['l33']))));
+        $this->slide5['i18'] = ($this->slide5['c3'] == 2 ? $this->slide4['o23'] + $this->slide4['o34'] : ($this->slide5['b3'] == 1 ? $this->slide4['m23'] + $this->slide4['m34'] : ($this->slide5['b4'] == 1 ? $this->slide4['n23'] + $this->slide4['n34'] : ($this->slide4['l23'] + $this->slide4['l34']))));
+        $this->slide5['i19'] = ($this->slide5['c3'] == 2 ? $this->slide4['o24'] : ($this->slide5['b3'] == 1 ? $this->slide4['m24']  : ($this->slide5['b4'] == 1 ? $this->slide4['n24'] : ($this->slide4['l24'] ))));
+        $this->slide5['i20'] = ($this->slide5['c3'] == 2 ? $this->slide4['o25'] : ($this->slide5['b3'] == 1 ? $this->slide4['m25']  : ($this->slide5['b4'] == 1 ? $this->slide4['n25'] : ($this->slide4['l25'] ))));
+        $this->slide5['i21'] = ($this->slide5['c3'] == 2 ? $this->slide4['o26'] : ($this->slide5['b3'] == 1 ? $this->slide4['m26']  : ($this->slide5['b4'] == 1 ? $this->slide4['n26'] : ($this->slide4['l26'] ))));
+        $this->slide5['i22'] = ($this->slide5['c3'] == 2 ? $this->slide4['o27'] : ($this->slide5['b3'] == 1 ? $this->slide4['m27']  : ($this->slide5['b4'] == 1 ? $this->slide4['n27'] : ($this->slide4['l27'] ))));
+        $this->slide5['i23'] = ($this->slide5['c3'] == 2 ? $this->slide4['o28'] : ($this->slide5['b3'] == 1 ? $this->slide4['m28']  : ($this->slide5['b4'] == 1 ? $this->slide4['n28'] : ($this->slide4['l28'] ))));
+        $this->slide5['i24'] = ($this->slide5['c3'] == 2 ? $this->slide4['o29'] + $this->slide4['o35'] : ($this->slide5['b3'] == 1 ? $this->slide4['m29'] + $this->slide4['m35'] : ($this->slide5['b4'] == 1 ? $this->slide4['n29'] + $this->slide4['n35'] : ($this->slide4['l29'] + $this->slide4['l35']))));
+
+//      Аллергии-ГКИ-ИСМП-0
+        $this->slide5['i29'] = ($this->slide5['c3'] == 2 ? $this->slide4['o39'] + $this->slide4['o47'] : ($this->slide5['b3'] == 1 ? $this->slide4['m39'] + $this->slide4['m47'] : ($this->slide5['b4'] == 1 ? $this->slide4['n39'] + $this->slide4['n47'] : ($this->slide4['l39'] + $this->slide4['l47']))));
+        $this->slide5['i30'] = ($this->slide5['c3'] == 2 ? $this->slide4['o40'] + $this->slide4['o48'] : ($this->slide5['b3'] == 1 ? $this->slide4['m40'] + $this->slide4['m48'] : ($this->slide5['b4'] == 1 ? $this->slide4['n40'] + $this->slide4['n48'] : ($this->slide4['l40'] + $this->slide4['l48']))));
+        $this->slide5['i31'] = ($this->slide5['c3'] == 2 ? $this->slide4['o41'] : ($this->slide5['b3'] == 1 ? $this->slide4['m41']  : ($this->slide5['b4'] == 1 ? $this->slide4['n41'] : ($this->slide4['l41'] ))));
+        $this->slide5['i32'] = ($this->slide5['c3'] == 2 ? $this->slide4['o42'] + $this->slide4['o49'] : ($this->slide5['b3'] == 1 ? $this->slide4['m42'] + $this->slide4['m49'] : ($this->slide5['b4'] == 1 ? $this->slide4['n42'] + $this->slide4['n49'] : ($this->slide4['l42'] + $this->slide4['l49']))));
+        $this->slide5['i33'] = ($this->slide5['c3'] == 2 ? $this->slide4['o43'] + $this->slide4['o50'] : ($this->slide5['b3'] == 1 ? $this->slide4['m43'] + $this->slide4['m50'] : ($this->slide5['b4'] == 1 ? $this->slide4['n43'] + $this->slide4['n50'] : ($this->slide4['l43'] + $this->slide4['l50']))));
+        $this->slide5['i34'] = ($this->slide5['c3'] == 2 ? $this->slide4['o44'] + $this->slide4['o51'] : ($this->slide5['b3'] == 1 ? $this->slide4['m44'] + $this->slide4['m51'] : ($this->slide5['b4'] == 1 ? $this->slide4['n44'] + $this->slide4['n51'] : ($this->slide4['l44'] + $this->slide4['l51']))));
+        $this->slide5['i35'] = ($this->slide5['c3'] == 2 ? $this->slide4['o45'] : ($this->slide5['b3'] == 1 ? $this->slide4['m45']  : ($this->slide5['b4'] == 1 ? $this->slide4['n45'] : ($this->slide4['l45'] ))));
+        $this->slide5['i36'] = ($this->slide5['c3'] == 2 ? $this->slide4['o46'] + $this->slide4['o52'] : ($this->slide5['b3'] == 1 ? $this->slide4['m46'] + $this->slide4['m52'] : ($this->slide5['b4'] == 1 ? $this->slide4['n46'] + $this->slide4['n52'] : ($this->slide4['l46'] + $this->slide4['l52']))));
+
+//      0-ГКИ-0-Специализация
+        $this->slide5['i42'] = ($this->slide5['c3'] == 2 ? $this->slide4['o55'] + $this->slide4['o63'] : ($this->slide5['b3'] == 1 ? $this->slide4['m55'] + $this->slide4['m63'] : ($this->slide5['b4'] == 1 ? $this->slide4['n55'] + $this->slide4['n63'] : ($this->slide4['l55'] + $this->slide4['l63']))));
+        $this->slide5['i43'] = ($this->slide5['c3'] == 2 ? $this->slide4['o56'] + $this->slide4['o64'] : ($this->slide5['b3'] == 1 ? $this->slide4['m56'] + $this->slide4['m64'] : ($this->slide5['b4'] == 1 ? $this->slide4['n56'] + $this->slide4['n64'] : ($this->slide4['l56'] + $this->slide4['l64']))));
+        $this->slide5['i44'] = ($this->slide5['c3'] == 2 ? $this->slide4['o57'] + $this->slide4['o65'] : ($this->slide5['b3'] == 1 ? $this->slide4['m57'] + $this->slide4['m65'] : ($this->slide5['b4'] == 1 ? $this->slide4['n57'] + $this->slide4['n65'] : ($this->slide4['l57'] + $this->slide4['l65']))));
+        $this->slide5['i45'] = ($this->slide5['c3'] == 2 ? $this->slide4['o58'] : ($this->slide5['b3'] == 1 ? $this->slide4['m58']  : ($this->slide5['b4'] == 1 ? $this->slide4['n58'] : ($this->slide4['l58'] ))));
+        $this->slide5['i46'] = ($this->slide5['c3'] == 2 ? $this->slide4['o59'] : ($this->slide5['b3'] == 1 ? $this->slide4['m59']  : ($this->slide5['b4'] == 1 ? $this->slide4['n59'] : ($this->slide4['l59'] ))));
+        $this->slide5['i47'] = ($this->slide5['c3'] == 2 ? $this->slide4['o60'] : ($this->slide5['b3'] == 1 ? $this->slide4['m60']  : ($this->slide5['b4'] == 1 ? $this->slide4['n60'] : ($this->slide4['l60'] ))));
+        $this->slide5['i48'] = ($this->slide5['c3'] == 2 ? $this->slide4['o61'] : ($this->slide5['b3'] == 1 ? $this->slide4['m61']  : ($this->slide5['b4'] == 1 ? $this->slide4['n61'] : ($this->slide4['l61'] ))));
+        $this->slide5['i49'] = ($this->slide5['c3'] == 2 ? $this->slide4['o62'] : ($this->slide5['b3'] == 1 ? $this->slide4['m62']  : ($this->slide5['b4'] == 1 ? $this->slide4['n62'] : ($this->slide4['l62'] ))));
+
+//      Аллергии-ГКИ-0-0
+        $this->slide5['i53'] = ($this->slide5['c3'] == 2 ? $this->slide4['o68'] + $this->slide4['o74'] : ($this->slide5['b3'] == 1 ? $this->slide4['m68'] + $this->slide4['m74'] : ($this->slide5['b4'] == 1 ? $this->slide4['n68'] + $this->slide4['n74'] : ($this->slide4['l68'] + $this->slide4['l74']))));
+        $this->slide5['i54'] = ($this->slide5['c3'] == 2 ? $this->slide4['o69'] + $this->slide4['o75'] : ($this->slide5['b3'] == 1 ? $this->slide4['m69'] + $this->slide4['m75'] : ($this->slide5['b4'] == 1 ? $this->slide4['n69'] + $this->slide4['n75'] : ($this->slide4['l69'] + $this->slide4['l75']))));
+        $this->slide5['i70'] = ($this->slide5['c3'] == 2 ? $this->slide4['o70'] + $this->slide4['o76'] : ($this->slide5['b3'] == 1 ? $this->slide4['m70'] + $this->slide4['m76'] : ($this->slide5['b4'] == 1 ? $this->slide4['n70'] + $this->slide4['n76'] : ($this->slide4['l70'] + $this->slide4['l76']))));
+        $this->slide5['i56'] = ($this->slide5['c3'] == 2 ? $this->slide4['o71'] + $this->slide4['o77'] : ($this->slide5['b3'] == 1 ? $this->slide4['m71'] + $this->slide4['m77'] : ($this->slide5['b4'] == 1 ? $this->slide4['n71'] + $this->slide4['n77'] : ($this->slide4['l71'] + $this->slide4['l77']))));
+        $this->slide5['i57'] = ($this->slide5['c3'] == 2 ? $this->slide4['o72'] + $this->slide4['o78'] : ($this->slide5['b3'] == 1 ? $this->slide4['m72'] + $this->slide4['m78'] : ($this->slide5['b4'] == 1 ? $this->slide4['n72'] + $this->slide4['n78'] : ($this->slide4['l72'] + $this->slide4['l78']))));
+        $this->slide5['i58'] = ($this->slide5['c3'] == 2 ? $this->slide4['o73'] + $this->slide4['o79'] : ($this->slide5['b3'] == 1 ? $this->slide4['m73'] + $this->slide4['m79'] : ($this->slide5['b4'] == 1 ? $this->slide4['n73'] + $this->slide4['n79'] : ($this->slide4['l73'] + $this->slide4['l79']))));
+
+//      0-0-ИСМП-Специализация
+        $this->slide5['i65'] = ($this->slide5['c3'] == 2 ? $this->slide4['o83'] + $this->slide4['o90'] : ($this->slide5['b3'] == 1 ? $this->slide4['m83'] + $this->slide4['m90'] : ($this->slide5['b4'] == 1 ? $this->slide4['n83'] + $this->slide4['n90'] : ($this->slide4['l83'] + $this->slide4['l90']))));
+        $this->slide5['i66'] = ($this->slide5['c3'] == 2 ? $this->slide4['o84'] + $this->slide4['o91'] : ($this->slide5['b3'] == 1 ? $this->slide4['m84'] + $this->slide4['m91'] : ($this->slide5['b4'] == 1 ? $this->slide4['n84'] + $this->slide4['n91'] : ($this->slide4['l84'] + $this->slide4['l91']))));
+        $this->slide5['i67'] = ($this->slide5['c3'] == 2 ? $this->slide4['o85'] : ($this->slide5['b3'] == 1 ? $this->slide4['m85']  : ($this->slide5['b4'] == 1 ? $this->slide4['n85'] : ($this->slide4['l85'] ))));
+        $this->slide5['i68'] = ($this->slide5['c3'] == 2 ? $this->slide4['o86'] : ($this->slide5['b3'] == 1 ? $this->slide4['m86']  : ($this->slide5['b4'] == 1 ? $this->slide4['n86'] : ($this->slide4['l86'] ))));
+        $this->slide5['i69'] = ($this->slide5['c3'] == 2 ? $this->slide4['o87'] : ($this->slide5['b3'] == 1 ? $this->slide4['m87']  : ($this->slide5['b4'] == 1 ? $this->slide4['n87'] : ($this->slide4['l87'] ))));
+        $this->slide5['i70'] = ($this->slide5['c3'] == 2 ? $this->slide4['o88'] : ($this->slide5['b3'] == 1 ? $this->slide4['m88']  : ($this->slide5['b4'] == 1 ? $this->slide4['n88'] : ($this->slide4['l88'] ))));
+        $this->slide5['i71'] = ($this->slide5['c3'] == 2 ? $this->slide4['o89'] : ($this->slide5['b3'] == 1 ? $this->slide4['m89']  : ($this->slide5['b4'] == 1 ? $this->slide4['n89'] : ($this->slide4['l89'] ))));
+
+//      0-ГКИ-0-0
+        $this->slide5['i75'] = ($this->slide5['c3'] == 2 ? $this->slide4['o95'] + $this->slide4['o98'] : ($this->slide5['b3'] == 1 ? $this->slide4['m95'] + $this->slide4['m98'] : ($this->slide5['b4'] == 1 ? $this->slide4['n95'] + $this->slide4['n98'] : ($this->slide4['l95'] + $this->slide4['l98']))));
+        $this->slide5['i76'] = ($this->slide5['c3'] == 2 ? $this->slide4['o96'] + $this->slide4['o99'] : ($this->slide5['b3'] == 1 ? $this->slide4['m96'] + $this->slide4['m99'] : ($this->slide5['b4'] == 1 ? $this->slide4['n96'] + $this->slide4['n99'] : ($this->slide4['l96'] + $this->slide4['l99']))));
+        $this->slide5['i77'] = ($this->slide5['c3'] == 2 ? $this->slide4['o97'] + $this->slide4['o100'] : ($this->slide5['b3'] == 1 ? $this->slide4['m97'] + $this->slide4['m100'] : ($this->slide5['b4'] == 1 ? $this->slide4['n97'] + $this->slide4['n100'] : ($this->slide4['l97'] + $this->slide4['l100']))));
+
+//      Аллергии-0-0-0
+        $this->slide5['i82'] = ($this->slide5['c3'] == 2 ? $this->slide4['o104'] + $this->slide4['o108'] : ($this->slide5['b3'] == 1 ? $this->slide4['m104'] + $this->slide4['m108'] : ($this->slide5['b4'] == 1 ? $this->slide4['n104'] + $this->slide4['n108'] : ($this->slide4['l104'] + $this->slide4['l108']))));
+        $this->slide5['i83'] = ($this->slide5['c3'] == 2 ? $this->slide4['o105'] + $this->slide4['o109'] : ($this->slide5['b3'] == 1 ? $this->slide4['m105'] + $this->slide4['m109'] : ($this->slide5['b4'] == 1 ? $this->slide4['n105'] + $this->slide4['n109'] : ($this->slide4['l105'] + $this->slide4['l109']))));
+        $this->slide5['i84'] = ($this->slide5['c3'] == 2 ? $this->slide4['o106'] + $this->slide4['o110'] : ($this->slide5['b3'] == 1 ? $this->slide4['m106'] + $this->slide4['m110'] : ($this->slide5['b4'] == 1 ? $this->slide4['n106'] + $this->slide4['n110'] : ($this->slide4['l106'] + $this->slide4['l110']))));
+        $this->slide5['i85'] = ($this->slide5['c3'] == 2 ? $this->slide4['o107'] + $this->slide4['o111'] : ($this->slide5['b3'] == 1 ? $this->slide4['m107'] + $this->slide4['m111'] : ($this->slide5['b4'] == 1 ? $this->slide4['n107'] + $this->slide4['n111'] : ($this->slide4['l107'] + $this->slide4['l111']))));
+
+//      0-0-0-0
+        $this->slide5['i92'] = ($this->slide5['c3'] == 2 ? $this->slide4['o115'] + $this->slide4['o117'] : ($this->slide5['b3'] == 1 ? $this->slide4['m115'] + $this->slide4['m117'] : ($this->slide5['b4'] == 1 ? $this->slide4['n115'] + $this->slide4['n117'] : ($this->slide4['l115'] + $this->slide4['l117']))));
+        $this->slide5['i93'] = ($this->slide5['c3'] == 2 ? $this->slide4['o116'] + $this->slide4['o118'] : ($this->slide5['b3'] == 1 ? $this->slide4['m116'] + $this->slide4['m118'] : ($this->slide5['b4'] == 1 ? $this->slide4['n116'] + $this->slide4['n118'] : ($this->slide4['l116'] + $this->slide4['l118']))));
 
 
         return $this->slide5;
