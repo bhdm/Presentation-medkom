979809368d62978d0840a67c7e74c948e6049eb6
bhdm <bhd.m@ya.ru>
2016-11-01 11:17:46 +0300

    fixed form in slide2

--- View/slides/slide3.php
+++ View/slides/slide3.php
@@ -3,11 +3,17 @@
         $_SESSION['slide1']['b2'] = htmlspecialchars($_POST['countOfYear']);
     }
     if (isset($_POST['specialty'])){
+        $_POST['specialty']['spec'] = null;
         foreach ($_POST['specialty'] as $key=> $spec){
-            $_SESSION['slide1']['spec']['title'] = htmlspecialchars($spec);
-            $_SESSION['slide1']['spec']['value'] = htmlspecialchars((isset($_POST['value'][$key]) ? $_POST['value'][$key] : 0));
+            $_SESSION['slide1']['spec'][] = array(
+                'title' => htmlspecialchars($spec),
+                'value' => htmlspecialchars((isset($_POST['value'][$key]) ? $_POST['value'][$key] : 0)),
+            );
+
         }
     }
+
+
 ?>
 <div class="title"><h1>Алгоритм расчета структуры закупок хирургических перчаток для операций</h1></div>
 
